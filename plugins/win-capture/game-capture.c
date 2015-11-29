359bf07f8be24a40978527eeceafecb80784eb69
Palana <palana@stunned.de>
2015-11-29 15:39:26 +0100

    win-capture: Actually start capture with PID/TID set

--- plugins/win-capture/game-capture.c
+++ plugins/win-capture/game-capture.c
@@ -352,7 +352,8 @@ static void game_capture_update(void *data, obs_data_t *settings)
 
 	free_config(&gc->config);
 	gc->config = cfg;
-	gc->activate_hook = !!window && !!*window;
+	gc->activate_hook = cfg.process_id || cfg.thread_id ||
+		(!!window && !!*window);
 	gc->retry_interval = DEFAULT_RETRY_INTERVAL;
 
 	if (!gc->initial_config) {
@@ -1274,7 +1275,8 @@ static bool start_capture(struct game_capture *gc)
 
 static inline bool capture_valid(struct game_capture *gc)
 {
-	if (!gc->dwm_capture && !IsWindow(gc->window))
+	if (!gc->process_id && gc->thread_id &&
+			!gc->dwm_capture && !IsWindow(gc->window))
 	       return false;
 	
 	return !object_signalled(gc->target_process);
